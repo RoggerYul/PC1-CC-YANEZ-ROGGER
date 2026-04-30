47ea97577b9ccc26093fac8c398c10735aaa0a31
Rogger Yañez. <84107451+RoggerYul@users.noreply.github.com>
2026-04-29 22:20:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ conn = psycopg2.connect(
     database="postgres",
     user="postgres",
     password="yul141534$",
-    port=5432
+    port="5432"
 )
 
 # =========================================
